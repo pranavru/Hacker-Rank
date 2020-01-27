4593a8279f8120f1778175f28f129f27d258741b
Pranav Ruparelia <pranavruparelia9@gmail.com>
2020-01-27 16:07:40 -0500

    Create CompareNodeValuesLL.java
    Multiple Test cases are provided with Linked List with the Node values.
    I had to traverse the Linked List and compare those values and return the values based on the comparision.

---
+++ DataStructures/CompareNodeValuesLL.java
@@ -0,0 +1,81 @@
+import java.io.*;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+        while (node != null) {
+            bufferedWriter.write(String.valueOf(node.data));
+
+            node = node.next;
+
+            if (node != null) {
+                bufferedWriter.write(sep);
+            }
+        }
+    }
+
+    // Complete the compareLists function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+    static boolean compareLists(SinglyLinkedListNode head1, SinglyLinkedListNode head2) {
+
+        SinglyLinkedListNode curr1 = head1;
+        SinglyLinkedListNode curr2 = head2;
+        boolean value = false;
+        while (curr1.next != null && curr2.next != null) {
+            if(curr1.data == curr2.data ) {
+                curr1 = curr1.next;
+                curr2 = curr2.next; 
+                value = true;
+            } else {
+                return false;
+            }
+        }
+        return value;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
