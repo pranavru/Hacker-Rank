2c49cb6367a5288534eb5b23168d6336dd2d4400
Pranav Ruparelia <pranavruparelia9@gmail.com>
2020-02-08 13:08:52 -0500

    Update SeriesCreation.java
    Creating a series of values using the given test cases.
    The series contains n elements defined by values a, b, and n
    
    Hence, sum of every element is series is ∑ a+ ((2^ i) + b) where i runs from 1 - n
    I have used while loop to initiate the sequence and stored it in a buffer such that i could avoid a space requirements of an array as well as reduce the time required to print the sequence

--- Java/SeriesCreation.java
+++ Java/SeriesCreation.java
@@ -1,1 +1,28 @@
+import java.util.*;
+import java.io.*;
+import java.lang.StringBuffer;
+
+class Solution{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        int sum = 0;
+        StringBuffer sb = new StringBuffer();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            int j = 0;
+            sum = a;
+            while(j < n) {
+                sum += (Math.pow(2, j) * b);
+                sb.insert(sb.length(), sum + " ");
+                j++;
+            }
+        System.out.println(sb);
+        sb.delete(0, sb.length());
+        }
+        in.close();
+    }
+}
 
