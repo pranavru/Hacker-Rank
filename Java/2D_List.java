cccd5ec5347ab401dd0a3113d6d605c0c24cef1d
Pranav Ruparelia <pranavruparelia9@gmail.com>
2020-03-26 12:16:09 -0400

    2D_List.java
    The given problem contains a list -> "arr" of type List<List<Integer>> needs to be traversed to find the absolute sum of the diagonals of the matrix and return the sum.

---
+++ Java/2D_List.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'diagonalDifference' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int diagonalDifference(List<List<Integer>> arr) {
+    // Write your code here
+        int sumDiag1 = 0, sumDiag2 = 0, sum = 0, n = arr.get(0).size();         
+        for(int i = 0; i < arr.size(); i++) {
+            sumDiag1 += arr.get(i).get(i);
+            sumDiag2 += arr.get(i).get(n -i -1);
+        }
+        sum = sumDiag1 > sumDiag2 ? sumDiag1 -sumDiag2 : sumDiag2 -sumDiag1;
+        return sum;
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<List<Integer>> arr = new ArrayList<>();
+
+        IntStream.range(0, n).forEach(i -> {
+            try {
+                arr.add(
+                    Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                        .map(Integer::parseInt)
+                        .collect(toList())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        int result = Result.diagonalDifference(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
